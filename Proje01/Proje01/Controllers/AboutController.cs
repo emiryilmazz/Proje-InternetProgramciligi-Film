06cad15ee3e0586319f4eddf699cdd65127025e7
emiryilmazz <emirylmzd@gmail.com>
2021-04-28 16:35:04 +0300

    update

---
+++ Proje01/Proje01/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proje01.Models.Siniflar;
+
+
+namespace Proje01.Controllers
+
+
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ Proje01/Proje01/Models/Siniflar/İletisim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proje01.Models.Siniflar
+{
+    public class İletisim
+    {
+        public int ID { get; set; }
+        public string AdSoyad { get; set; }
+        public string Mail { get; set; }
+        public string Konu { get; set; }
+        public string Mesaj { get; set; }
+
+    }
+}
